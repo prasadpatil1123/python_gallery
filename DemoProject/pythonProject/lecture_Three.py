cea1a3df0b6741693a62bca41b970de0efd21850
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2024-04-01 23:38:11 +0530

    first commit

---
+++ DemoProject/pythonProject/lecture_Three.py
@@ -0,0 +1,20 @@
+# student = ["Kashi",95.4,17,"Pune"]
+# print(student)
+# print(student[0])
+# print(student[5])#Error
+
+# student[0]="Prasad"
+# print(student)
+
+# str = "Ram"
+# print(str[0])
+# str[0]="Sh"
+# print(str)#Error
+
+marks = [70,85,90,45,65]
+# print(marks[1:4])
+# print(marks[-3:-1])
+# print(marks[1:4])
+# print(marks[0:5])
+# print(marks[1:len(marks)])
+print(marks[:4])
